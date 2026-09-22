b92142cb6b2ce27d43066a4676ac92fe1066659e
rory <rorymreardon@gmail.com>
2026-09-21 17:38:03 -0700

    Fix formatting of fulladd instance in adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -16,7 +16,7 @@ module adder4(
    fulladd fa0 (.x(a[0]), .y(b[0]), .cin(1'b0), .sum(result[0]), .cout(c1));
    fulladd fa1 (.x(a[1]), .y(b[1]), .cin(c1),   .sum(result[1]), .cout(c2));
    fulladd fa2 (.x(a[2]), .y(b[2]), .cin(c2),   .sum(result[2]), .cout(c3));
-   fulladd fa3 (.x(a[3]), .y(b[3]), .cin(c3),   .sum(result[3]), .cout(result[4]));
+	fulladd fa3 (.x(a[3]), .y(b[3]), .cin(c3),   .sum(result[3]), .cout(result[4])); 
 	
 endmodule
 
