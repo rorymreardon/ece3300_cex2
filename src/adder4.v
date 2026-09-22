86557d6c2673e44debd72bbcce98e733ab966493
rory <rorymreardon@gmail.com>
2026-09-21 17:26:29 -0700

    Implement 4-bit adder using full adders

--- src/adder4.v
+++ src/adder4.v
@@ -13,7 +13,11 @@ module adder4(
 	      output [4:0] result);
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
-   
+   fulladd fa0 (.x(a[0]), .y(b[0]), .cin(1'b0), .sum(result[0]), .cout(c1));
+   fulladd fa1 (.x(a[1]), .y(b[1]), .cin(c1),   .sum(result[1]), .cout(c2));
+   fulladd fa2 (.x(a[2]), .y(b[2]), .cin(c2),   .sum(result[2]), .cout(c3));
+   fulladd fa3 (.x(a[3]), .y(b[3]), .cin(c3),   .sum(result[3]), .cout(result[4]));
+	
 endmodule
 
 module fulladd(
